66267b3ea02a26cb4a966326b8e9397635ecd53d
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-09-09 00:43:36 +0200

    Style Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
+//style
 import './App.css';
 
+//components
 import NavButton from './Components/NavButton.jsx';
 import NavigationMenu from './Components/NavigationMenu.jsx';
 
+//pages
 import Home from './pages/Home.jsx';
 import Work from './pages/Work.jsx';
 import About from './pages/About.jsx';
@@ -14,7 +17,7 @@ import Contact from './pages/Contact.jsx';
 
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);  //state 
 
 
   return (
