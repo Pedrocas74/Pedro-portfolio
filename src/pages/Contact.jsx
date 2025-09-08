b8b74185bae5973faa719feced4f753d773577d7
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-09-08 15:23:58 +0200

    Create Contact page prototype

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,19 @@
+
 import styles from '../styles/contact.module.css';
 
 function Contact() {
 
     return(
-        <>
-        <span>This will be CONTACT page</span>
-        </>
+        <div className={styles.contactPage}>
+            <h1 className={styles.title}>CONTACT</h1>
+            <div className={styles.contacts}>
+                <a href="mailto:pmig.magalhaes@gmail.com">Send me an email</a>
+                <a href="">Github</a>
+                <a href="">Linkedin</a>
+
+            </div>
+            
+        </div>
     );
 }
 
